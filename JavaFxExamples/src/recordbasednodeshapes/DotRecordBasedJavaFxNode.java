bf9de9ec24396dff9ed1341946c6e0ae200ab505
Zoey Gerrit Prigge <zoey.prigge@uni-duesseldorf.de>
2017-09-25 15:48:57 +0200

    wip2

--- JavaFxExamples/src/recordbasednodeshapes/DotRecordBasedJavaFxNode.java
+++ JavaFxExamples/src/recordbasednodeshapes/DotRecordBasedJavaFxNode.java
@@ -19,130 +19,125 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
-public /*abstract*/ class DotRecordBasedJavaFxNode extends HBox {
+public /* abstract */ class DotRecordBasedJavaFxNode extends HBox {
 
-	
-	private static Border RECORD_BORDER = new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(0), BorderWidths.DEFAULT));
-	private static Border MRECORD_BORDER = new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT));
-	private static Insets TEXT_MARGINS = new Insets(0, 5, 0, 5);
+	private static Border RECORD_BORDER = new Border(
+			new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(0), BorderWidths.DEFAULT));
+	private static Border MRECORD_BORDER = new Border(
+			new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT));
+	private static Insets TEXT_MARGINS = new Insets(1.5f, 7, 1.5f, 7);
 
-	
-	public DotRecordBasedJavaFxNode(/*String dotLabel, boolean useRoundedBorders*/) {
-		RootNode(RECORD_BORDER);
+	protected DotRecordBasedJavaFxNode(/* String dotLabel, boolean useRoundedBorders */) {
 	}
-	
+
 	/**
 	 * label = "A | B | C | D"
 	 */
 	public static DotRecordBasedJavaFxNode createExample01() {
-        DotRecordBasedJavaFxNode node = new RootNode();
-        
-		node.addText("A");
-        node.getChildren().add(node.new RecordBasedLabelLine(Orientation.HORIZONTAL));
-		node.addText("B");
-        node.getChildren().add(node.new RecordBasedLabelLine(Orientation.HORIZONTAL));
-		node.addText("C");
-        node.getChildren().add(node.new RecordBasedLabelLine(Orientation.HORIZONTAL));
-		node.addText("D");
+		RootNode root = new RootNode();
 
-        return node;
+		root.addText(" ");
+		// root.addLine();
+		// root.addText("B");
+		// root.addLine();
+		// root.addText("C");
+		// root.addLine();
+		// root.addText("D");
+
+		return root;
 	}
 
-
 	/**
-	 * label = "{A | B | C | D}" 
+	 * label = "{A | B | C | D}"
 	 */
 	public static DotRecordBasedJavaFxNode createExample02() {
-		DotRecordBasedJavaFxNode node = new RootNode();
-		
-		VLabelNode vbox = node.new VLabelNode();
-        
-		node.getChildren().add(vbox);
+		RootNode node = new RootNode();
+
+		LabelNode vbox = node.provideChildNode();
+
+		node.addRotatedLabel(vbox);
 
 		vbox.addText("A");
-		vbox.addLine();
-		vbox.addText("B");
-        vbox.addLine();
-		vbox.addText("C");
-		vbox.addLine();
-		vbox.addText("D");
-		
-        return node;
+		// vbox.addLine();
+		// vbox.addText("B");
+		// vbox.addLine();
+		// vbox.addText("C");
+		// vbox.addLine();
+		// vbox.addText("D");
+
+		return node;
 	}
 
 	/**
 	 * label = "A | { B | C } | D"
 	 */
 	public static DotRecordBasedJavaFxNode createExample03() {
-		LabelNode node = new RootNode();
-		
-		VLabelNode vbox = node.new VLabelNode();
-        
+		RootNode node = new RootNode();
+
+		LabelNode vbox = node.provideChildNode();
+
 		node.addText("A");
 		node.addLine();
-		node.getChildren().add(vbox);
+		node.addRotatedLabel(vbox);
 		vbox.addText("B");
-        vbox.addLine();
+		vbox.addLine();
 		vbox.addText("C");
-        node.addLine();
+		node.addLine();
 		node.addText("D");
-		
-        return node;
+
+		return node;
 	}
 
 	/**
 	 * label = "<f0> left|<f1> mid&#92; dle|<f2> right"
 	 */
 	public static DotRecordBasedJavaFxNode createExample04_1() {
-DotRecordBasedJavaFxNode node = new RootNode();
-		
-		VLabelNode vbox = node.new VLabelNode();
-        
+		RootNode node = new RootNode();
+
 		node.addText("left");
 		node.addLine();
 		node.addText("mid dle");
-        node.addLine();
+		node.addLine();
 		node.addText("right");
-		
-        return node;
+
+		return node;
 	}
 
 	/**
 	 * label = "<f0> one|<f1> two"
 	 */
 	public static DotRecordBasedJavaFxNode createExample04_2() {
-		DotRecordBasedJavaFxNode node = new RootNode();
-		
-		VLabelNode vbox = node.new VLabelNode();
-        
+		RootNode node = new RootNode();
+
 		node.addText("one");
 		node.addLine();
 		node.addText("two");
-		
-        return node;
+
+		return node;
 	}
 
 	/**
 	 * label = "hello&#92;nworld |{ b |{c|<here> d|e}| f}| g | h"
 	 */
 	public static DotRecordBasedJavaFxNode createExample04_3() {
-		DotRecordBasedJavaFxNode node = new RootNode(MRECORD_BORDER);
-		
-		VLabelNode vbox = node.new VLabelNode();
-		DotRecordBasedJavaFxNode hbox = new DotRecordBasedJavaFxNode();
-        
+		RootNode node = new RootNode(MRECORD_BORDER);
+
+		LabelNode vbox = node.provideChildNode();
+		LabelNode hbox = vbox.provideChildNode();
+
 		node.addText("hello&#92;nworld");
 		node.addLine();
-		node.getChildren().add(vbox);
+		node.addRotatedLabel(vbox);
 		vbox.addText("b");
-        vbox.addLine();
-        vbox.getChildren().add(hbox);
+		vbox.addLine();
+		vbox.addRotatedLabel(hbox);
 		hbox.addText("c");
 		hbox.addLine();
 		hbox.addText("d");
@@ -154,25 +149,24 @@ DotRecordBasedJavaFxNode node = new RootNode();
 		node.addText("g");
 		node.addLine();
 		node.addText("h");
-		
-        return node;
+
+		return node;
 	}
-	
+
 	/**
-	 * Mrecord
-	 * label = "hello&#92;nworld |{ b |{c|<here> d|e}| f}| g | h"
+	 * Mrecord label = "hello&#92;nworld |{ b |{c|<here> d|e}| f}| g | h"
 	 */
 	public static DotRecordBasedJavaFxNode createExample05_3() {
-		LabelNode node = new RootNode();
+		RootNode node = new RootNode();
 		LabelNode vbox = node.provideChildNode();
 		LabelNode hbox = node.provideChildNode();
-        
+
 		node.addText("hello&#92;nworld");
 		node.addLine();
-		node.getChildren().add(vbox);
+		node.addRotatedLabel(vbox);
 		vbox.addText("b");
-        vbox.addLine();
-        vbox.getChildren().add(hbox);
+		vbox.addLine();
+		vbox.addRotatedLabel(hbox);
 		hbox.addText("c");
 		hbox.addLine();
 		hbox.addText("d");
@@ -184,17 +178,16 @@ DotRecordBasedJavaFxNode node = new RootNode();
 		node.addText("g");
 		node.addLine();
 		node.addText("h");
-		
-        return node;
+
+		return node;
 	}
-	
-	
-    // TODO: define further cerateExample... methods
-	
+
+	// TODO: define further cerateExample... methods
+
 	private class RecordBasedLabelLine extends Region {
 		Line line = new Line();
 		BiConsumer<Double, Double> lineResize;
-		
+
 		public RecordBasedLabelLine(Orientation orientation) {
 			getChildren().add(line);
 			switch (orientation != null ? orientation : Orientation.HORIZONTAL) {
@@ -205,50 +198,52 @@ DotRecordBasedJavaFxNode node = new RootNode();
 			default:
 				lineResize = (width, height) -> line.setEndY((double) height);
 				break;
-			}	
+			}
 		}
-		
+
 		@Override
 		public void resize(double width, double height) {
 			lineResize.accept(Double.valueOf(width), Double.valueOf(height));
 			super.resize(width, height);
-		}		
+		}
 	}
-	
 
-	
 	private interface LabelNode {
 		public void addText(String string);
-		public void addLine();
+
+		// public void addLine();
 		public void addRotatedLabel(LabelNode label);
+
 		public LabelNode provideChildNode();
 	}
-	
+
 	private static class RootNode extends HLabelNode {
 		public RootNode() {
-	        this(RECORD_BORDER);
+			this(RECORD_BORDER);
 		}
-		
+
 		public RootNode(Border border) {
-	        setBorder(border);
-	        setMinHeight(35);
-	        setMaxWidth(0);
-		}	
+			setBorder(border);
+			setMinHeight(35);
+			setMaxWidth(0);
+		}
+
+		@Override
+		public void addRotatedLabel(LabelNode label) {
+			setHgrow((Node) label, Priority.ALWAYS);
+			setPrefWidth(35);
+			setMaxWidth(35);
+			setMinHeight(0);
+			getChildren().add((Node) label);
+		}
+
 	}
-	
+
 	private static class HLabelNode extends DotRecordBasedJavaFxNode implements LabelNode {
+		boolean firstField = true;
+
 		public HLabelNode() {
-	        setAlignment(Pos.CENTER);
-		}
-		
-		public void addText(String string) {
-			Text text = new Text(string);
-			setMargin(text, TEXT_MARGINS);
-	        getChildren().add(text);
-		}
-		
-		public void addLine() {
-			getChildren().add(new RecordBasedLabelLine(Orientation.HORIZONTAL));
+			setAlignment(Pos.CENTER);
 		}
 
 		@Override
@@ -256,37 +251,68 @@ DotRecordBasedJavaFxNode node = new RootNode();
 			return new VLabelNode();
 		}
 
-		@Override
-		public void addRotatedLabel(LabelNode label) {
-			//can't do this any different due to polymorphism not being possible.
-			getChildren().add((Node) label);
-		}
-	}
-	
-	private class VLabelNode extends VBox implements LabelNode {	
-		public VLabelNode() {
-	        setAlignment(Pos.CENTER);
-		}
-		
 		public void addText(String string) {
+			firstFieldCheck();
 			Text text = new Text(string);
 			setMargin(text, TEXT_MARGINS);
 	        getChildren().add(text);
 		}
-		
-		public void addLine() {
-			getChildren().add(new RecordBasedLabelLine(Orientation.VERTICAL));
+
+		@Override
+		public void addRotatedLabel(LabelNode label) {
+			firstFieldCheck();
+			getChildren().add((Node) label);
 		}
-		
+
+		private void firstFieldCheck() {
+			if (!firstField)
+				addLine();
+			else
+				firstField = false;
+		}
+
+		private void addLine() {
+			getChildren().add(new RecordBasedLabelLine(Orientation.HORIZONTAL));
+		}
+
+	}
+
+	private class VLabelNode extends VBox implements LabelNode {
+		boolean firstField = true;
+
+		public VLabelNode() {
+			setAlignment(Pos.CENTER);
+		}
+
 		@Override
 		public LabelNode provideChildNode() {
 			return new HLabelNode();
 		}
 
+		public void addText(String string) {
+			firstFieldCheck();
+			Text text = new Text(string);
+			setMargin(text, TEXT_MARGINS);
+			getChildren().add(text);
+		}
+
 		@Override
 		public void addRotatedLabel(LabelNode label) {
-			getChildren().add((Node) label);			
+			firstFieldCheck();
+			getChildren().add((Node) label);
 		}
+
+		private void firstFieldCheck() {
+			if (!firstField)
+				addLine();
+			else
+				firstField = false;
+		}
+
+		private void addLine() {
+			getChildren().add(new RecordBasedLabelLine(Orientation.VERTICAL));
+		}
+
 	}
-	
+
 }
